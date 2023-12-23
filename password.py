a7ab5f535ebc000b0ac0e8012ae1f0bc15f6348f
Diego_Dev <147381532+poysongnocchi@users.noreply.github.com>
2023-12-23 22:07:41 +1100

    Added user input interface loop

---
+++ password.py
@@ -0,0 +1,10 @@
+def choice_menu():
+    while True:
+        # Display Menu
+        print("\nPassword Manager")
+        print("1. Show Saved Passwords")
+        print("2. Add New Password")
+        print("3. Generate Strong Password")
+        print("4. Exit App")
+
+        choice = input("Enter your choice: ")
